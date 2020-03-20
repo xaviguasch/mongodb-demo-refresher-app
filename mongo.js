01037496881c6373ca61fdc9994a44b7a1129e4f
xaviguasch <xaviguasch@gmail.com>
2020-03-20 23:27:04 +0100

    created a document with mongodb

--- mongo.js
+++ mongo.js
@@ -1,9 +1,26 @@
 const MongoClient = require('mongodb').MongoClient
 
 const url =
-  'mongodb+srv://xavi:<password>@cluster0-o4vnl.mongodb.net/products_test?retryWrites=true&w=majority'
+  'mongodb+srv://xavi:<PASSWORD>@cluster0-o4vnl.mongodb.net/product_test?retryWrites=true&w=majority'
 
-const createProduct = async (req, res, next) => {}
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  }
+  const client = new MongoClient(url)
+
+  try {
+    await client.connect()
+    const db = client.db()
+    const result = db.collection('products').insertOne(newProduct)
+  } catch (error) {
+    return res.json({ message: 'Could not store data.' })
+  }
+  client.close()
+
+  res.json(newProduct)
+}
 
 const getProducts = async (req, res, next) => {}
 
